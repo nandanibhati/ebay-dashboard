26e3a04fe979b08b72c800f19871318a9389eda9
Nandini bhati <Nandinibhati451@gmail.com>
2026-06-18 15:10:33 +0530

    model

--- backend/models/Leave.js
+++ backend/models/Leave.js
@@ -21,27 +21,4 @@ const leaveSchema = new mongoose.Schema(
 module.exports = mongoose.model(
   "Leave",
   leaveSchema
-);const mongoose = require("mongoose");
-
-const leaveSchema = new mongoose.Schema(
-  {
-    employeeName: String,
-    employeeEmail: String,
-
-    fromDate: String,
-    toDate: String,
-
-    reason: String,
-
-    status: {
-      type: String,
-      default: "Pending",
-    },
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model(
-  "Leave",
-  leaveSchema
 );
